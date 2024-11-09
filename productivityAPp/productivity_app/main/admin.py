e184fc1be46b7b07e483e395547a646c3493886d
PrasadSaha1 <161397110+PrasadSaha1@users.noreply.github.com>
2024-11-09 08:23:59 -0500

    Delete productivityAPp/productivity_app/main directory

--- productivityAPp/productivity_app/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import PomodoroTimer
-from .models import TodoList
-
-# Register your models here.
-admin.site.register(PomodoroTimer)
-admin.site.register(TodoList)
